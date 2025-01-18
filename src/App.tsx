135400825ef2c7052dc4adc9cbfc106c2c2cdde6
ibrahimRabeh <Ibrahim.rabehh@gmail.com>
2025-01-18 14:10:25 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Check, Code, Cpu, ExternalLink, Github, Lightbulb, Linkedin, Mail, Moon, Sun, Terminal,  } from 'lucide-react';
+import Projects from './sections.tsx';
+
+
+const App = () => {
+ return (
+   <>
+     <Projects />
+   </>
+ );
+};
+
+export default App;
+
